ffe8e6ada209bce8002132b543f4202dd41582f3
bill-c2018 <wcunning81@gmail.com>
2021-03-05 13:34:32 -0800

    user tokens

---
+++ src/main/java/com/billsbars/app/service/UserAuthenticationServiceImp.java
@@ -0,0 +1,13 @@
+package com.billsbars.app.service;
+
+import com.billsbars.app.model.Token;
+
+public class UserAuthenticationServiceImp implements UserAuthenticationService {
+
+	@Override
+	public boolean isUserAdmin(Token token) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
